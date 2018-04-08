d2a0dc692a99f8020b101208c6a4ff1fe293283d
greatghoul <greatghoul@gmail.com>
2018-04-08 18:29:00 +0800

    load weekly total period

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,18 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-console.log(`'Allo 'Allo! Popup`)
+import $ from 'jquery'
+
+function extractTotal (html) {
+  const doc = $($.parseHTML(html))
+  const total = doc.find('.weekly-reports-table:first .tfoot .duration:last').text()
+  $('#duration').text(total)
+}
+
+function fetchTotal () {
+  window.fetch('https://app.hubstaff.com/reports/my/weekly?', { credentials: 'include' })
+    .then(resp => resp.text())
+    .then(html => extractTotal(html))
+}
+
+fetchTotal()
